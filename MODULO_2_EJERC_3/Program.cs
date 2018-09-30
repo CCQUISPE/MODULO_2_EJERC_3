16dcaf4373e22a2a77dbcc09cfe0c5a51ab28929
cami63830 <cami63830@gmail.com>
2018-09-29 21:03:29 -0300

    si

--- MODULO_2_EJERC_3/Program.cs
+++ MODULO_2_EJERC_3/Program.cs
@@ -10,6 +10,15 @@ namespace MODULO_2_EJERC_3
     {
         static void Main(string[] args)
         {
+            //Tarea 3: Haz un programa que calcule el producto de los números 13 y 12.
+            int V3, V4;
+            V3 = 13;
+            V4 = 12;
+            Console.WriteLine("Ejercicio 3: el producto de 13x12= {0}", V3 * V4); // si queremos realizar el producto sin variables,
+            //deberemos colocar los valores en v3 y v4
+            Console.ReadKey();
+
+
         }
     }
 }
